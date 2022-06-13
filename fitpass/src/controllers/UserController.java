e03e75a41baec7452332d60df8771cca7c12dded
ognjens <svraka.ra181.2019@uns.ac.rs>
2022-06-13 10:06:31 +0200

    Initial setup

---
+++ fitpass/src/controllers/UserController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import static spark.Spark.delete;
+import static spark.Spark.get;
+import static spark.Spark.put;
+import static spark.Spark.post;
+
+public class UserController {
+
+    public static void test() {
+        get(
+            "rest/users/", (req,res) -> {
+                return "hello world";
+                }
+        );
+    }
+}
---
+++ fitpass/src/rest/SparkMainApp.java
@@ -0,0 +1,19 @@
+package rest;
+
+import controllers.UserController;
+
+import java.io.File;
+
+import static spark.Spark.port;
+import static spark.Spark.staticFiles;
+
+public class SparkMainApp {
+
+    public static void main(String[] args) throws Exception {
+        port(8080);
+
+        staticFiles.externalLocation(new File("./static").getCanonicalPath());
+
+        UserController.test();
+    }
+}
