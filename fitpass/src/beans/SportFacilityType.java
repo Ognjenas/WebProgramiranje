51a60cf3d5c02789bea327b5d9091985dba9600c
Lee <licina.luka@gmail.com>
2022-06-15 15:35:11 +0200

    Finished fetching AllFacilitiesDto

---
+++ fitpass/src/beans/SportFacilityType.java
@@ -0,0 +1,10 @@
+package beans;
+
+public enum SportFacilityType {
+    GYM,
+    FIGHTING_GYM,
+    SWIMMING_POOL,
+    SPORTS_CENTER,
+    DANCE_STUDIO,
+    STADIUM
+}
--- fitpass/src/controllers/SportFacilityController.java
+++ fitpass/src/controllers/SportFacilityController.java
@@ -1,6 +1,28 @@
 package controllers;
 
+import com.google.gson.Gson;
+import dto.AllFacilitiesDto;
+import services.SportFacilityService;
+
+import java.util.List;
+
+import static spark.Spark.delete;
+import static spark.Spark.get;
+import static spark.Spark.put;
+import static spark.Spark.post;
+
+
 public class SportFacilityController {
+    private static SportFacilityService facilityService = new SportFacilityService();
+    private static Gson g = new Gson();
 
-
+    public static void test() {
+        get("facilities/", (req, res) -> {
+                    res.type("application/json");
+                    String payload = req.body();
+                    AllFacilitiesDto s = SportFacilityService.getAllFacilities();
+                    return g.toJson(s);
+                }
+        );
+    }
 }
