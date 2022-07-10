6b47c8a43566237f4e4081c37749f26e457f325e
ognjens <svraka.ra181.2019@uns.ac.rs>
2022-07-10 21:05:15 +0200

    Locations implemented

--- fitpass/static/js/facilityShow.js
+++ fitpass/static/js/facilityShow.js
@@ -8,13 +8,23 @@ Vue.component("facility-show", {
                 }
 
             },
-            currentFacility: null,
+            currentFacility: {
+                location: {},
+                openTime: {
+                    startWorkingDays: {},
+                    endWorkingDays: {},
+                    startSaturday: {},
+                    endSaturday: {},
+                    startSunday: {},
+                    endSunday: {}
+                }
+            },
             id: "",
             userInfo: {
                 username: "",
                 role: ""
             },
-            offers : "",
+            offers: "",
             reserveOffer: {date: {}, time: "", id: ""},
             dateValue: ""
 
@@ -22,14 +32,18 @@ Vue.component("facility-show", {
     },
     template: ` 
 <div>
-<div v-if="$cookies.get('token') != null">
+    <div v-if="$cookies.get('token') != null">
         <label>Username: {{userInfo.username}}</label>
         <button v-on:click="editProfile">Profile</button>
     </div>
+    
     <div v-if="$cookies.get('token') == null">
         <button v-on:click="login">Login</button>
     </div>
+    
     <h1>SHOW FACILITY</h1>
+    <input type="hidden" id="geoLen" v-model="currentFacility.location.geoLength" onload="makeMap()">
+    <input type="hidden" id="geoWidth" v-model="currentFacility.location.geoWidth">
     <table>
         <tr>
         <td><label>Name</label></td>
@@ -49,7 +63,7 @@ Vue.component("facility-show", {
         <tr>
         <td><label>Location</label></td>
         <td> 
-            <p>Geo.Length:{{currentFacility.location.geoLength}} / Geo.Width:{{currentFacility.location.geoWidth}}</p>
+            <div id="map2" class="map"></div>
 		    <p>City:{{currentFacility.location.city}}-Street:{{currentFacility.location.street}}-Nr:{{currentFacility.location.strNumber}}-Postal:{{currentFacility.location.postalCode}}</p></td>
         </tr>
         
@@ -70,9 +84,12 @@ Vue.component("facility-show", {
 		    <p>Saturday: From {{currentFacility.openTime.startSaturday.hour}}:{{currentFacility.openTime.startSaturday.minute}} to {{currentFacility.openTime.endSaturday.hour}}:{{currentFacility.openTime.endSaturday.minute}}</p>
 		    <p>Sunday: From {{currentFacility.openTime.startSunday.hour}}:{{currentFacility.openTime.startSunday.minute}} to {{currentFacility.openTime.endSunday.hour}}:{{currentFacility.openTime.endSunday.minute}}</p></td>
         </tr>
+        
     </table>
     
+    
     <h1>Offers</h1>
+   
     <div v-for="offer in offers">
         <p>Name: {{offer.name}}</p>
         <p>Type: {{offer.type}}</p>
@@ -80,6 +97,7 @@ Vue.component("facility-show", {
         <p>Image: <img :src="offer.imgSource" width="100" height="100"></p>
         <button v-if="userInfo.role == 'CUSTOMER'" v-on:click="reserveButton(offer.id)">Reserve</button>
     </div>
+
 </div>		  
 `
     ,
@@ -92,7 +110,7 @@ Vue.component("facility-show", {
                 router.push("/edit-profile")
             },
             reserveButton(id) {
-                router.push("/facility-show/offer/"+id);
+                router.push("/facility-show/offer/" + id);
             }
         },
 
@@ -102,7 +120,19 @@ Vue.component("facility-show", {
             .get('/show-facility?id=' + this.$route.params.id)
             .then(response => {
                 this.currentFacility = response.data;
-        });
+                new ol.Map({
+                    target: 'map2',
+                    layers: [
+                        new ol.layer.Tile({
+                            source: new ol.source.OSM()
+                        })
+                    ],
+                    view: new ol.View({
+                        center: ol.proj.fromLonLat([this.currentFacility.location.geoLength, this.currentFacility.location.geoWidth]),
+                        zoom: 10
+                    })
+                });
+            });
 
         if ($cookies.get("token") != null) {
             axios.post('users/get-info', $cookies.get("token"), this.configHeaders)
@@ -116,6 +146,8 @@ Vue.component("facility-show", {
             .then(response => {
                 this.offers = response.data.offers;
             })
+
+
 
     },
 });
--- fitpass/static/js/openFacility.js
+++ fitpass/static/js/openFacility.js
@@ -8,14 +8,15 @@ Vue.component("open-facility", {
                 }
 
             },
-            currentFacility: {location: {},
+            currentFacility: {
+                location: {},
                 openTime: {
-                    startWorkingDays : {},
-                    endWorkingDays : {},
-                    startSaturday : {},
-                    endSaturday : {},
+                    startWorkingDays: {},
+                    endWorkingDays: {},
+                    startSaturday: {},
+                    endSaturday: {},
                     startSunday: {},
-                    endSunday : {}
+                    endSunday: {}
                 }
             },
             id: "",
@@ -35,8 +36,10 @@ Vue.component("open-facility", {
     <div v-if="$cookies.get('token') == null">
         <button v-on:click="login">Login</button>
     </div>
-    <h1>SHOW FACILITY</h1>
+    <h1>My facility</h1>
     <table>
+    <input type="hidden" id="geoLen2" v-model="currentFacility.location.geoLength">
+    <input type="hidden" id="geoWidth2" v-model="currentFacility.location.geoWidth">
         <tr>
         <td><label>Name</label></td>
         <td><label>{{currentFacility.name}}</label></td>
@@ -55,7 +58,7 @@ Vue.component("open-facility", {
         <tr>
         <td><label>Location</label></td>
         <td> 
-            <p>Geo.Length:{{currentFacility.location.geoLength}} / Geo.Width:{{currentFacility.location.geoWidth}}</p>
+            <div id="map3" class="map"></div>
 		    <p>City:{{currentFacility.location.city}}-Street:{{currentFacility.location.street}}-Nr:{{currentFacility.location.strNumber}}-Postal:{{currentFacility.location.postalCode}}</p></td>
         </tr>
         
@@ -98,6 +101,7 @@ Vue.component("open-facility", {
 	</tr>
 </table>
 </div>
+<div id="map2" class="map"></div>
 </div>		  
 `
     ,
@@ -116,7 +120,7 @@ Vue.component("open-facility", {
                 router.push("/open-facility/trainers")
             },
             editOffer(id) {
-                router.push("/open-facility/offers/"+id)
+                router.push("/open-facility/offers/" + id)
             },
             getTrainings() {
                 router.push("/open-facility/trainings")
@@ -140,11 +144,26 @@ Vue.component("open-facility", {
             .get('/manager/get-facility', this.configHeaders)
             .then(response => {
                 this.currentFacility = response.data;
+                new ol.Map({
+                    target: 'map3',
+                    layers: [
+                        new ol.layer.Tile({
+                            source: new ol.source.OSM()
+                        })
+                    ],
+                    view: new ol.View({
+                        center: ol.proj.fromLonLat([this.currentFacility.location.geoLength, this.currentFacility.location.geoWidth]),
+                        zoom: 10
+                    })
+
+                });
             });
 
         axios.get('/manager/get-facility-offers', this.configHeaders)
             .then(response => {
-            this.offers = response.data.offers;
-        });
+                this.offers = response.data.offers;
+            });
+
+
     },
 });
